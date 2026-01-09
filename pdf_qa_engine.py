e222de6d2ca57e27fd908aa00e30ac847969bb7f
pavan-koda <pavankumar.k@adroitecengg.com>
2026-01-09 12:18:55 +0530

    up

--- pdf_qa_engine.py
+++ pdf_qa_engine.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_ollama import ChatOllama
 from langchain.chains import RetrievalQA
